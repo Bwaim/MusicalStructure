d457ec5aa1632c3f191d65d4a9672b3182806205
Fabien Boismoreau <fabien.boismoreau@gmail.com>
2018-02-25 21:32:37 +0100

    feat: Add the Object Album

--- app/src/main/java/com/bwaim/musicalstructure/MainActivity.java
+++ app/src/main/java/com/bwaim/musicalstructure/MainActivity.java
@@ -16,14 +16,23 @@
 
 package com.bwaim.musicalstructure;
 
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MusicPagerAdapter mMusicPagerAdapter;
+    private ViewPager mViewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*mMusicPagerAdapter = new MusicPagerAdapter(getSupportFragmentManager());
+        mViewPager = findViewById(R.id.pager);
+        mViewPager.setAdapter(mMusicPagerAdapter);*/
     }
 }
